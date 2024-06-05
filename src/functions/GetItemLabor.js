5297109ded9a0b203df887434d648cadd6cf5964
Jolin Nguyen <jolin.nguyen@ict-campus.net>
2024-06-05 15:28:13 +0200

    changed db

--- src/functions/GetItemLabor.js
+++ src/functions/GetItemLabor.js
@@ -1,8 +1,8 @@
 const { app,input } = require('@azure/functions');
 
 const cosmosInput = input.cosmosDB({
-    databaseName: 'DemoDatabase',
-    containerName: 'Items',
+    databaseName: 'FallbspDB',
+    containerName: 'FallbspContainer',
     connection: 'CosmosDB',
     sqlQuery: "select * from c"
 });
